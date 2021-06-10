944335f21910c194f69e3c44b7a199bb61e98e69
bahrim remus <81024947+Bahrim00@users.noreply.github.com>
2021-06-10 20:26:28 +0300

    Initial main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+import Player
+import pygame
+pygame.init()
+pygame.mixer.init()
+
 
 introMsg = '''
 *************************************************
@@ -14,6 +19,9 @@ introMsg = '''
 *************************************************
 '''
 
+sound = 'Main_Menu.wav'
+sounda = pygame.mixer.Sound(sound)
+sounda.play()
 print(introMsg)
 print('Would you like to start the adventure ? ')
 user_answer = input('Yes or No -> ')
@@ -21,6 +29,44 @@ user_answer = input('Yes or No -> ')
 if user_answer.upper() == 'YES':
     print('OK')
     os.system('clear')
+    answer = input('''
+What type of player are you:
+1 for Warrior 2 for Wizzard -->  ''' )
+    if answer == '1':
+        player_name = input('''
+You have chosen to be a mighty Warrior
+What is your name? ->
+        ''')
+        player = Player.Warrior(player_name)
+        print('Intro to the world')
+        input('Press a key to continue... ')
+
+        os.system('clear')
+        sound = 'Exploring.wav'
+        sounda = sounda.stop()
+        sounda = pygame.mixer.Sound(sound)
+        sounda.play()
+        print('''You are in the middle of a crossroads
+You have 3 path in front of you:
+1 - Going to a village
+2 - Going to a forest
+3 - Going to a desert
+''')
+        path_option = input('What is your desteny? ')
+
+    elif answer == '2':
+        player_name = input('''
+You have chosen to be a mighty Wizard
+What is your name? ->
+        ''')
+        player = Player.Wizard(player_name)
+        print('Intro to the world')
+        os.system('clear')
+
+
+    else:
+        print('Choose a valid option')
+
 else:
     print('Thank you, Good BYE !')
 
